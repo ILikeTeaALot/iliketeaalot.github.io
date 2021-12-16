6d290738ebd3b9a74fdfd59b5ad5a656ba9c24a5
James Hart <james.s.hart@outlook.com>
2021-12-16 14:10:00 +0000

    convert to new Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Page } from "./components/Page";
 import "./fonts/fonts.css";
 
 interface Props {
@@ -44,7 +45,7 @@ export class App extends React.Component<Props, State> {
 			const intersectionRatio = entry.target.dataset.background === "#000000" ? 0.1 : 0.6;
 			if (entry.intersectionRatio >= 0.6) {
 				/// @ts-ignore
-				entry.target.parentElement.parentElement.style.opacity = 1;	
+				entry.target.parentElement.parentElement.style.opacity = 1;
 			} else {
 				/// @ts-ignore
 				entry.target.parentElement.parentElement.style.opacity = 0;
@@ -155,179 +156,110 @@ export class App extends React.Component<Props, State> {
 						<div><a onClick={this.doNothing} href="/about">about</a></div>
 					</div>
 					<div className="padding" aria-hidden />
-					<div className="page" /* style={{ opacity: this.state.showProjects ? 1 : 0 }} */>
-						<div className="project">
-							<div ref={this.sections[0]} data-background="#FFFFFF" className="project-info">
-								<h1 className="title">My Skills</h1>
-								<h4>2018 - Present</h4>
-								<h2 className="heading">Vertically Centring a Div</h2>
-								<p className="copy">It's often joked about how difficult it is to vertically centre an HTML "div" element. So here it is, right in front of you. Enjoy.</p>
-								<h2 className="heading">React</h2>
-								<p className="copy">As of December 2021, I have been using React for over 3 years, and have finished multiple projects.</p>
-								<h2 className="heading">React Native</h2>
-								<p className="copy">I also have about 18 months experience in React Native, as can be seen in MobileTrainTimes, and a number of smaller experiemental projects.</p>
-								<h2 className="heading">CSS</h2>
-								<p className="copy">I am highly skilled in CSS. Every projects' styles on this website were written from the ground up.</p>
-								<h2 className="heading">Git</h2>
-								<p className="copy">I have collaborated with multiple devlopers on several occasions.</p>
-								<h2 className="heading">Other Languages & Technologies</h2>
-								<ul>
-									<li>Node.JS Server+Desktop</li>
-									<li>C#/.NET Core</li>
-									<li>Rust</li>
-								</ul>
-							</div>
-							<div className="project-showcase">
-								<div>
-									<h2 className="title">Vertically Centred Div</h2>
-									<p className="copy">(Real HTML. Press F12 and open your inspector!)</p>
-									<div className="centre-outer">
-										<div className="centred" />
-									</div>
-								</div>
+					<Page background="#FFFFFF" colour="#000000" info={[
+						{ type: "title", content: "My Skills" },
+						{ type: "date", content: "2018 - Present" },
+						{ type: "heading", content: "Vertically Centring a Div" },
+						{ type: "copy", content: "It's often joked about how difficult it is to vertically centre an HTML \"div\" element. So here it is, right in front of you. Enjoy." },
+						{ type: "heading", content: "React" },
+						{ type: "copy", content: "As of December 2021, I have been using React for over 3 years, and have finished multiple projects." },
+						{ type: "heading", content: "React Native" },
+						{ type: "copy", content: "I also have about 18 months experience in React Native, as can be seen in MobileTrainTimes, and a number of smaller experiemental projects." },
+						{ type: "heading", content: "CSS" },
+						{ type: "copy", content: "I am highly skilled in CSS. Every projects' styles on this website were written from the ground up." },
+						{ type: "heading", content: "Git" },
+						{ type: "copy", content: "I have collaborated with multiple devlopers on several occasions." },
+						{ type: "heading", content: "Other Languages & Technologies" },
+						{ type: "list", content: ["Node.JS Server+Desktop", "C#/.NET Core", "Rust"] },
+					]}>
+						<div>
+							<h2 className="title">Vertically Centred Div</h2>
+							<p className="copy">(Real HTML. Press F12 and open your inspector!)</p>
+							<div className="centre-outer">
+								<div className="centred" />
 							</div>
 						</div>
-					</div>
-					<div className="page" /* style={{ opacity: this.state.showProjects ? 1 : 0 }} */>
-						{/* <div>
-							<h1 className="heading">My work</h1>
-						</div> */}
-						<div className="project">
-							<div ref={this.sections[1]} data-background="#FFFFFF" className="project-info">
-								<h1 className="title">June</h1>
-								<h4>July - Winter 2020</h4>
-								<h2 className="heading">About</h2>
-								{/* <p className="copy">In 2006, Microsoft released the Zune. It was an interesting take on the portable music player, and by 2008 the visual style had evolved into what many today would recognise as "Metro", Microsoft's attempt at an ultra-minimalist user interface/experience. Zune was perhaps the best implmentation of the Metro design language, with a look that still feels modern and timeless today, 12 years later.</p>
-								<p className="copy">However, the Zune devices were discontinued in 2011, and the desktop software stopped receiving updates in 2012.</p> */}
-								<p className="copy">In 2020, I took Zune as a design start-point, and attempted to build my own music playing software that could stand toe-to-toe with iTunes and WinAmp.</p>
-								<p className="copy">In many ways, I succeeded: the Windows version could play multiple tracks seamlessly, and on both Windows and Mac OS the music database was reliable and highly performant. The User Interface is stunningly clean and simple, but due to the single-threaded and poor-performance nature of Javascript+Electron, playing music and attempting to animate the UI simultaneously resulted in a muddied mess.</p>
-								<h2 className="heading">Frameworks</h2>
-								<h3 className="heading">Desktop:</h3>
-								<ul>
-									<li>React</li>
-									<li>Electron</li>
-									<li>Node.JS</li>
-								</ul>
-								<h3 className="heading">Mobile:</h3>
-								<ul>
-									<li>React Native</li>
-								</ul>
-								<h2 className="heading">Platforms</h2>
-								<ul>
-									<li>Windows</li>
-									<li>Mac OS (Prototype)</li>
-									<li>Linux (Theoretical - Untested)</li>
-									<li>Android (Prototype)</li>
-									<li>iOS (Prototype)</li>
-								</ul>
-								<h2 className="heading">Notable Libraries/APIs Used</h2>
-								<ul>
-									<li>SQLite</li>
-								</ul>
-							</div>
-							<div className="project-showcase">
-								<section>
-									<h2 className="title">Website Concept</h2>
-									<p className="copy">(Click to enlarge)</p>
-									<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Website Concept.png")?.default} />
-								</section>
-								<section>
-									<h2 className="title">Mobile Concepts</h2>
-									<p className="copy">(Click to enlarge)</p>
-									<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Mobile Concept/Home.png")?.default} />
-									<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Mobile Concept/Now Playing.png")?.default} />
-									<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Mobile Concept/Quickplay - With Pins.png")?.default} />
-								</section>
-								<section>
-									<h2 className="title">Desktop Application</h2>
-								</section>
-							</div>
-						</div>
-					</div>
-					<div className="page" /* style={{ opacity: this.state.showProjects ? 1 : 0 }} */>
-						<div className="project">
-							<div ref={this.sections[2]} data-background="#FFFFFF" className="project-info">
-								<h1 className="title">MobileTrainTimes</h1>
-								<h4>August - December 2020</h4>
-								<h2 className="heading">About</h2>
-								<p className="copy">In addition to computers, I have been interested in trains for about as long as I can remember. MobileTrainTimes is an amalgamation of two of those two interests; It is designed to be the simplest, most elegant rail journey planning and information app available.</p>
-								<h2 className="heading">Frameworks</h2>
-								<ul>
-									<li>React Native</li>
-								</ul>
-								<h2 className="heading">Platforms</h2>
-								<ul>
-									<li>Android</li>
-									<li>iOS</li>
-								</ul>
-								<h2 className="heading">Notable Libraries/APIs Used</h2>
-								<ul>
-									<li>Huxley 2 - For interfacing with the National Rail Enquiries "Darwin" system.</li>
-								</ul>
-							</div>
-							<div className="project-showcase">
-								<div style={{ height: "200vh" }} />
-							</div>
-						</div>
-					</div>
-					<div className="page" /* style={{ opacity: this.state.showProjects ? 1 : 0 }} */>
-						<div className="project">
-							<div ref={this.sections[3]} data-background="#000000" data-color="#FFFFFF" className="project-info">
-								<h1 className="title">IsItWeekA.com</h1>
-								<h4>Dec 2020 - Present</h4>
-								<h2 className="heading">About</h2>
-								{/* <p className="copy">Following a tradition going back over 10 years now, a student at my secondary school has managed the domain and website "isitweeka.com", and in 2020 that honour passed to a friend of mine, and I was asked to design the website. I also tasked myself with implementing a Google-free method of interfacing with the school's calendar, for privacy and maintainability purposes.</p> */}
-								<p className="copy">At the end of 2020, a friend of mine became the latest student of my secondary school to own and manage the domain "isitweeka.com".</p>
-								<p className="copy">He asked me to modernise and redesign the website, as well as help implement a number of new features.</p>
-								<h2 className="heading">Frameworks</h2>
-								<ul>
-									<li>React</li>
-								</ul>
-								<h2 className="heading">Platforms</h2>
-								<ul>
-									<li>Web</li>
-								</ul>
-								<h2 className="heading">Notable Libraries/APIs Used</h2>
-								<ul>
-									<li>Raw iCal parsing.</li>
-								</ul>
-							</div>
-							<div className="project-showcase">
-								<h2 className="title">Main Landing Page</h2>
-								<img tabIndex={1} className="software-image concept-image" src={require("./screenshots/IsItWeekA/main.png")?.default} />
-							</div>
-						</div>
-					</div>
-					<div className="page" /* style={{ opacity: this.state.showProjects ? 1 : 0 }} */>
-						<div className="project">
-							<div ref={this.sections[4]} data-background="#FFC118" className="project-info">
-								<h1 className="title">Drezr</h1>
-								<h4>Dec 2018 - Jan 2019</h4>
-								<h2 className="heading">About</h2>
-								<p className="copy">In December 2018, I was asked if I'd like to help with a startup idea. I jumped at the opportunity, being particularly young and naïve at the time.</p>
-								<p className="copy">I was given a detailed mockup of the site and finished an early prototype within 24 hours, although it was only a simple single-page website.</p>
-								<p className="copy">As of some time in late 2019/early 2020, the site is down.</p>
-								<h2 className="heading">Frameworks</h2>
-								<ul>
-									<li>React</li>
-									<li>Vanilla HTML/CSS/JS</li>
-								</ul>
-								<h2 className="heading">Platforms</h2>
-								<ul>
-									<li>Web</li>
-								</ul>
-								<h2 className="heading">Notable Libraries/APIs Used</h2>
-								<ul>
-									<li>PHP Backend for handling email form.</li>
-								</ul>
-							</div>
-							<div className="project-showcase">
-								<h2 className="title">Main Landing Page</h2>
-								<p className="copy">(Click to enlarge)</p>
-								{/* <img tabIndex={1} className="software-image concept-image" src={require("./screenshots/IsItWeekA/main.png")?.default} /> */}
-							</div>
-						</div>
-					</div>
+					</Page>
+					<Page background="#FFFFFF" colour="#000000" info={[
+						{ type: "title", content: "June" },
+						{ type: "date", content: "July - Winter 2020" },
+						{ type: "heading", content: "About" },
+						{ type: "copy", content: "In 2020, I took Zune as a design start-point, and attempted to build my own music playing software that could stand toe-to-toe with iTunes and WinAmp." },
+						{ type: "copy", content: "In many ways, I succeeded: the Windows version could play multiple tracks seamlessly, and on both Windows and Mac OS the music database was reliable and highly performant. The User Interface is stunningly clean and simple, but due to the single-threaded and poor-performance nature of Javascript+Electron, playing music and attempting to animate the UI simultaneously resulted in a muddied mess." },
+						{ type: "heading", content: "Frameworks" },
+						{ type: "subheading", content: "Desktop:" },
+						{ type: "list", content: ["React", "Electron", "Node.js"] },
+						{ type: "subheading", content: "Mobile:" },
+						{ type: "list", content: ["React Native"] },
+						{ type: "heading", content: "Platforms" },
+						{ type: "list", content: ["Windows", "Mac OS (Prototype)", "Linux (Theoretical - Untested)", "Android (Prototype)", "iOS (Prototype)"] },
+						{ type: "heading", content: "Notable Libraries/APIs Used" },
+						{ type: "list", content: ["SQLite"] },
+					]}>
+						<section>
+							<h2 className="title">Website Concept</h2>
+							<p className="copy">(Click to enlarge)</p>
+							<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Website Concept.png")?.default} />
+						</section>
+						<section>
+							<h2 className="title">Mobile Concepts</h2>
+							<p className="copy">(Click to enlarge)</p>
+							<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Mobile Concept/Home.png")?.default} />
+							<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Mobile Concept/Now Playing.png")?.default} />
+							<img tabIndex={1} className="software-image concept-image" src={require("./concepts/June/Mobile Concept/Quickplay - With Pins.png")?.default} />
+						</section>
+						<section>
+							<h2 className="title">Desktop Application</h2>
+						</section>
+					</Page>
+					<Page background="#FFFFFF" colour="#000000" info={[
+						{ type: "title", content: "MobileTrainTimes" },
+						{ type: "date", content: "August - December 2020" },
+						{ type: "heading", content: "About" },
+						{ type: "copy", content: "In addition to computers, I have been interested in trains for about as long as I can remember. MobileTrainTimes is an amalgamation of two of those two interests; It is designed to be the simplest, most elegant rail journey planning and information app available." },
+						{ type: "heading", content: "Frameworks" },
+						{ type: "list", content: ["React Native"] },
+						{ type: "heading", content: "Platforms" },
+						{ type: "list", content: ["Android", "iOS"] },
+						{ type: "heading", content: "Notable Libraries/APIs Used" },
+						{ type: "list", content: ["Huxley 2 - For interfacing with the National Rail Enquiries \"Darwin\" system."] },
+					]}>
+						<div style={{ height: "200vh" }} />
+					</Page>
+					<Page background="#000000" colour="#FFFFFF" info={[
+						{ type: "title", content: "IsItWeekA.com" },
+						{ type: "date", content: "Dec 2020 - Present" },
+						{ type: "heading", content: "About" },
+						{ type: "copy", content: "At the end of 2020, a friend of mine became the latest student of my secondary school to own and manage the domain \"isitweeka.com\"." },
+						{ type: "copy", content: "He asked me to modernise and redesign the website, as well as help implement a number of new features." },
+						{ type: "heading", content: "Frameworks" },
+						{ type: "list", content: ["React"] },
+						{ type: "heading", content: "Platforms" },
+						{ type: "list", content: ["Web"] },
+						{ type: "heading", content: "Notable Libraries/APIs Used" },
+						{ type: "list", content: ["Raw iCal Parsing."] },
+					]}>
+						<h2 className="title">Main Landing Page</h2>
+						<img tabIndex={1} className="software-image concept-image" src={require("./screenshots/IsItWeekA/main.png")?.default} />
+					</Page>
+					<Page background="#FFC118" colour="#000000" info={[
+						{ type: "title", content: "Drezr" },
+						{ type: "date", content: "Dec 2018 - Jan 2019" },
+						{ type: "heading", content: "About" },
+						{ type: "copy", content: "In December 2018, I was asked if I'd like to help with a startup idea. I jumped at the opportunity, being particularly young and naïve at the time." },
+						{ type: "copy", content: "I was given a detailed mockup of the site and finished an early prototype within 24 hours, although it was only a simple single-page website." },
+						{ type: "copy", content: "Unfortuntely, as of some time in late 2019/early 2020, the site is down." },
+						{ type: "heading", content: "Frameworks" },
+						{ type: "list", content: ["React", "Vanilla HTML/CSS/JS"] },
+						{ type: "heading", content: "Platforms" },
+						{ type: "list", content: ["Web"] },
+						{ type: "heading", content: "Notable Libraries/APIs Used" },
+						{ type: "list", content: ["PHP Backend for handling email form."] },
+					]}>
+						<h2 className="title">Main Landing Page</h2>
+						<p className="copy">(Click to enlarge)</p>
+						{/* <img tabIndex={1} className="software-image concept-image" src={require("./screenshots/IsItWeekA/main.png")?.default} /> */}
+					</Page>
 				</div>
 			</>
 		);

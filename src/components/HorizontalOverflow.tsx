9ad0c62163be137b8caf985160f61c2a3ce507d2
James Hart <james.s.hart@outlook.com>
2021-12-18 18:02:23 +0000

    component-ise css demos

---
+++ src/components/HorizontalOverflow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const HorizontalOverflow: React.FunctionComponent = () => {
+	return (
+		<div></div>
+	)
+}
---
+++ src/components/VerticallyCentredDiv.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const VCDiv: React.FunctionComponent = () => {
+	return (
+		<div className="centre-outer">
+			<div className="centred" />
+		</div>
+	)
+}

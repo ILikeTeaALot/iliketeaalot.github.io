54b709b81158d6b00587d419359c524fc9cfdd66
James Hart <james.s.hart@outlook.com>
2021-12-31 14:40:23 +0000

    add decoration to ListNav

--- src/components/NewListNav/ListNav.tsx
+++ src/components/NewListNav/ListNav.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import "./style.css";
 
-export const ListNav: React.FunctionComponent = (props) => {
+interface Props {
+	show?: boolean;
+	onClose?: React.MouseEventHandler;
+	onClick?: React.MouseEventHandler;
+}
+
+export const ListNav: React.FunctionComponent<Props> = (props) => {
 	return (
-		<div className="list-nav">
-			<ul>
+		<div className="list-nav" style={{ opacity: props.show ? 1 : 0 }} onClick={props.onClick}>
+			<div id="nav-top"></div>
+			<ul style={{ pointerEvents: props.show ? "all" : "none" }}>
 				{props.children}
 			</ul>
+			<div id="nav-bottom"></div>
 		</div>
-	)
-}
+	);
+};

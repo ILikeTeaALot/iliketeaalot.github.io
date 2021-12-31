d647d0193f6a16924a2ce226c708ddcc296f61ee
James Hart <james.s.hart@outlook.com>
2021-12-31 14:40:34 +0000

    add onClick to ListNavItem

--- src/components/NewListNav/ListNavItem.tsx
+++ src/components/NewListNav/ListNavItem.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-export const ListNav: React.FunctionComponent = (props) => {
+interface Props {
+	accent?: string;
+	to: string;
+	onClick?: React.MouseEventHandler;
+}
+
+export const ListNavItem: React.FunctionComponent<Props> = (props) => {
+	const handleClick: React.MouseEventHandler = (e) => {
+		e.preventDefault();
+		if (props.onClick) { props.onClick(e); return; }
+		// const y = getComputedStyle(document.documentElement).getPropertyValue("--y-position");
+		// console.log(y)
+		// console.log((document.querySelector(props.to)?.getBoundingClientRect().y || 0) - parseInt(y));
+		// window.scrollTo({ behavior: "smooth", top: document.querySelector(props.to)?.getBoundingClientRect().y });
+		document.querySelector(props.to)?.scrollIntoView({ behavior: "smooth" });
+	};
+
 	return (
-		<li>
-			<a>{props.children}</a>
+		<li onClick={handleClick}>
+			<a href={props.to}>{props.children}</a>
 		</li>
 	);
 };
